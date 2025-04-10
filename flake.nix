e58c075cc775e2c4765dae40b5b41764ec1ac3ed
Ed Fawcett-Taylor <ed.fawcett-taylor@proton.me>
2025-04-10 09:12:41 +0100

    use crane

--- flake.nix
+++ flake.nix
@@ -1,35 +1,85 @@
 {
-  description = "A very basic rust flake";
+  description = "Build a cargo project without extra checks";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, ... }:
-    let
-      system = "x86_64-linux";
-
-      overlays = [ (import rust-overlay) ];
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
 
       pkgs = import nixpkgs {
         inherit system overlays;
-        config = {
-          allowUnfree = true;
-        };
       };
 
       rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-    in
-    with pkgs;
-    {
-      devShell.${system} = mkShell {
-        buildInputs = [ rustToolchain ];
+
+      craneLib = (crane.mkLib pkgs).overrideToolchain (p: rustToolchain);
+      # Common arguments can be set here to avoid repeating them later
+      # Note: changes here will rebuild all dependency crates
+      commonArgs = {
+        src = craneLib.cleanCargoSource ./.;
+        strictDeps = true;
+
+        buildInputs = [
+          # Add additional build inputs here
+        ];
       };
-    };
+
+      my-crate = craneLib.buildPackage (commonArgs
+        // {
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+          # Additional environment variables or build phases/hooks can be set
+          # here *without* rebuilding all dependency crates
+          # MY_CUSTOM_VAR = "some value";
+        });
+
+      moldDevShell = craneLib.devShell.override {
+        # For example, use the mold linker
+        mkShell = pkgs.mkShell.override {
+          stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+        };
+      };
+    in {
+      checks = {
+        inherit my-crate;
+      };
+
+      packages.default = my-crate;
+
+      apps.default = flake-utils.lib.mkApp {
+        drv = my-crate;
+      };
+
+      devShells.default = moldDevShell {
+        # Inherit inputs from checks.
+        checks = self.checks.${system};
+
+        # Additional dev-shell environment variables can be set directly
+        # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+        # Extra inputs can be added here; cargo and rustc are provided by default.
+        packages = [
+          # pkgs.ripgrep
+        ];
+      };
+    });
 }
