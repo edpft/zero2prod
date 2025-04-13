02fc268ee3edbaa8222114643ee547392c91a58d
Ed Fawcett-Taylor <ed.fawcett-taylor@proton.me>
2025-04-13 10:36:42 +0100

    messing about with `flake.nix`

--- flake.nix
+++ flake.nix
@@ -3,9 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
     crane.url = "github:ipetkov/crane";
-
     flake-utils.url = "github:numtide/flake-utils";
 
     rust-overlay = {
@@ -29,57 +27,66 @@
         inherit system overlays;
       };
 
+      # Use the rust version specified in our `rust-toolchain.toml`
       rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
 
-      craneLib = (crane.mkLib pkgs).overrideToolchain (p: rustToolchain);
-      # Common arguments can be set here to avoid repeating them later
-      # Note: changes here will rebuild all dependency crates
+      src = craneLib.cleanCargoSource ./.;
+      nativeBuildInputs = [rustToolchain];
+
       commonArgs = {
-        src = craneLib.cleanCargoSource ./.;
+        inherit src nativeBuildInputs;
         strictDeps = true;
-
-        buildInputs = [
-          # Add additional build inputs here
-        ];
       };
 
-      my-crate = craneLib.buildPackage (commonArgs
-        // {
-          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-          # Additional environment variables or build phases/hooks can be set
-          # here *without* rebuilding all dependency crates
-          # MY_CUSTOM_VAR = "some value";
+      cargoClippyExtraArgs = "--all-targets -- --warn clippy::pedantic --deny warnings";
+      clippy = craneLib.cargoClippy (commonArgs
+        // {
+          inherit cargoArtifacts cargoClippyExtraArgs;
         });
 
-      moldDevShell = craneLib.devShell.override {
-        # For example, use the mold linker
-        mkShell = pkgs.mkShell.override {
-          stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+      nextest = craneLib.cargoNextest (commonArgs
+        // {
+          inherit cargoArtifacts;
+          partitions = 1;
+          partitionType = "count";
+          cargoNextestPartitionsExtraArgs = "--no-tests=pass";
+        });
+
+      bin = craneLib.buildPackage (commonArgs
+        // {
+          inherit cargoArtifacts;
+        });
+
+      dockerImage = pkgs.dockerTools.buildImage {
+        name = "zero2prod";
+        tag = "latest";
+        copyToRoot = [bin];
+        config = {
+          Cmd = ["${bin}/bin/zero2prod"];
         };
       };
+
+      moldDevShell = with pkgs;
+        craneLib.devShell.override {
+          mkShell = mkShell.override {
+            stdenv = stdenvAdapters.useMoldLinker stdenv;
+          };
+        };
     in {
       checks = {
-        inherit my-crate;
+        inherit bin clippy nextest;
       };
 
-      packages.default = my-crate;
-
-      apps.default = flake-utils.lib.mkApp {
-        drv = my-crate;
+      packages = {
+        inherit bin dockerImage;
+        default = bin;
       };
 
       devShells.default = moldDevShell {
-        # Inherit inputs from checks.
-        checks = self.checks.${system};
-
-        # Additional dev-shell environment variables can be set directly
-        # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-
-        # Extra inputs can be added here; cargo and rustc are provided by default.
-        packages = [
-          # pkgs.ripgrep
-        ];
+        inputsFrom = [bin];
       };
     });
 }
