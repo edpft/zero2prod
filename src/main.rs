6590db8a7b6f0e0b8f9f2ab863eb296eff9accba
Ed Fawcett-Taylor <ed.fawcett-taylor@proton.me>
2025-04-13 10:53:32 +0100

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
-fn main() {
-    let name = "world";
-    println!("Hello, {name}!");
+use std::net::Ipv4Addr;
+
+use axum::{Router, extract::Path, response::IntoResponse, routing};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", routing::get(greet_world))
+        .route("/{name}", routing::get(greet_individual));
+
+    let host = Ipv4Addr::UNSPECIFIED;
+    let port = 8080u16;
+    let socket_address = (host, port);
+
+    let listener = tokio::net::TcpListener::bind(socket_address).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn greet_world() -> impl IntoResponse {
+    "Hello, world!"
+}
+
+async fn greet_individual(Path(name): Path<String>) -> impl IntoResponse {
+    format!("Hello, {name}!")
 }
